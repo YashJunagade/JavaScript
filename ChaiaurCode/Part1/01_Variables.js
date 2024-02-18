51044955274e1a80d5ef0d814a98635dc8381148
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-02-18 10:03:58 +0530

    Variables in JS

---
+++ ChaiaurCode/Part1/01_Variables.js
@@ -0,0 +1,10 @@
+const id = 1 // constant variable declared with the const keyword (we cannot change the value of the constant variable)
+
+// Variables are declared with the two methods using let and var (mostly we use let because var doesn't follow the scope rules)
+let name = "Yash" // variable declared with the let keyword
+var age = 21 // variable declared with the var keyword
+let add;
+
+city = "Nashik" // we can also create without using any keyword but its a bad practice (don't declare variables in this way)
+
+console.table([id, name, age, add, city]); //console.table is used to print the variables in tabular form
