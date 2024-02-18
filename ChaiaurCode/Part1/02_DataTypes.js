576b41fa680f85f43a105ac24e73e42f0411ca5a
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-02-18 10:04:17 +0530

    Data Types

---
+++ ChaiaurCode/Part1/02_DataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+/* Data Types in JS
+
+- Primitive data types: number, string, boolean, symbol, null and undefined.
+    Number 
+    String => "", '' (we can use both single quote and double quote)
+    Boolean => true/false
+    null => standalone value
+    undefined => value is not declared
+    symbol => used for unique identifiers
+    BigInt => used for large numbers
+
+- Reference data types: array and object
+    object => {key:value}
+*/
+
+
+let name = "Yash" 
+let age = 19 
+let adult = true 
