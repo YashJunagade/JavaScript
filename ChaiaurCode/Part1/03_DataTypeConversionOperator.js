06b8820e126b5f44487fa6b9dd0cd79168371b71
Yash Anil Junagade <yash.junagade1900@gmail.com>
2024-02-19 14:16:13 +0530

    add Operations in JS

--- ChaiaurCode/Part1/03_DataTypeConversionOperator.js
+++ ChaiaurCode/Part1/03_DataTypeConversionOperator.js
@@ -1,17 +1,66 @@
 /* typeof() & typeof <variable_name> we can use both method to know the data type of the variable*/
 
-let score = "33";
-score = Number(score) // convert string to number
-console.log(score);
-console.log(typeof(score));
+// let score = "33";
+// score = Number(score) // convert string to number
+// console.log(score);
+// console.log(typeof(score));
 
-let score2 = 33;
-score2 = String(score2) // convert number to string
-console.log(score2);
-console.log(typeof(score2));
+// let score2 = 33;
+// score2 = String(score2) // convert number to string
+// console.log(score2);
+// console.log(typeof(score2));
 
 
-let flag = 1;
-flag = Boolean(flag) // convert number to boolean
-console.log(flag);
-console.log(typeof(flag));
+// let flag = 1;
+// flag = Boolean(flag) // convert number to boolean
+// console.log(flag);
+// console.log(typeof(flag));
+
+// *********************** Operations ***********************
+
+/* Operations in JS
+    - Arithmetic operations: +, -, *, / (division), % (modulus), ** (exponent)
+    - Increment and Decrement operators ++x, --x
+    - Assignment operators +=, -=, *=, /=, %=, <<=, >>=, >>>=, &=, ^=, |=
+    - Comparison operators ==, ===, !=, !==, >, <, >=, <=
+    - Logical operators &&, ||, !
+*/
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
